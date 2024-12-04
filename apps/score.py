ff0cc281289921ba2f2b42810ee5ec29cf44b8e7
Jeff Perry <jsp@utexas.edu>
2024-12-04 08:10:11 -0600

    Make scoring faster: Do math by hand, don't call scikit

--- apps/score.py
+++ apps/score.py
@@ -6,15 +6,14 @@ compute metrics.
 import argparse
 import pandas as pd
 import glob
+import math
 import sys
 from sklearn.metrics import accuracy_score
-from sklearn.metrics import balanced_accuracy_score
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import f1_score
-from sklearn.metrics import matthews_corrcoef
 
 
-def score_all(c, a, r, d, headers=False):
+def score_all(c, a, y, d, headers=False):
 
     if headers is True:
         print(f'Cls'
@@ -26,10 +25,10 @@ def score_all(c, a, r, d, headers=False):
 
     # Get the scores
     p = d[a]
-    acc = accuracy_score(r, p)
-    weighted_f1 = f1_score(r, p, average="weighted")
-    macro_f1 = f1_score(r, p, average="macro")
-    micro_f1 = f1_score(r, p, average="micro")
+    acc = accuracy_score(y, p)
+    weighted_f1 = f1_score(y, p, average="weighted")
+    macro_f1 = f1_score(y, p, average="macro")
+    micro_f1 = f1_score(y, p, average="micro")
     print(f'{c}'
           f'\t{a}'
           f'\t{acc:0.3f}'
@@ -38,13 +37,15 @@ def score_all(c, a, r, d, headers=False):
           f'\t{micro_f1:0.3f}')
 
 
-def score_binary(c, a, ref, df, pos_label, headers=False):
+def score_binary(c, a, y, df, pos_label, headers=False):
 
     # Replace values
-    r = ref.copy()
+    r = y.copy()
     d = df.copy()
     r[r != pos_label] = 0
     d[d != pos_label] = 0
+    r[r != pos_label] = 1
+    d[d != pos_label] = 1
 
     if headers is True:
         print(f'Cls'
@@ -59,17 +60,21 @@ def score_binary(c, a, ref, df, pos_label, headers=False):
 
     # Get the scores
     p = d[a]
-    acc = accuracy_score(r, p)
-    f1 = f1_score(r, p, pos_label=pos_label)
-    ba = balanced_accuracy_score(r, p)
-    mcc = matthews_corrcoef(r, p)
-    cm = confusion_matrix(r, p)
-    TP = cm[0][0]
-    FN = cm[0][1]
-    FP = cm[1][0]
-    TN = cm[1][1]
+    TN, FP, FN, TP = confusion_matrix(r, p).ravel()
+    PP = TP + FP
+    PN = TN + FN
     TPR = TP / (TP + FN)
     FPR = FP / (FP + TN)
+    TNR = 1.0 - FPR
+    FNR = 1.0 - TPR
+    PPV = TP / PP
+    NPV = TN / PN
+    FOR = FN / PN
+    FDR = FP / PP
+    acc = (TP + TN) / (TP + TN + FP + FN)
+    f1 = (2*TP) / (2*TP + FP + FN)
+    ba = (TPR + TNR) / 2.0
+    mcc = math.sqrt(TPR * TNR * PPV * NPV) - math.sqrt(FNR * FPR * FOR * FDR)
     r0 = 0.5
     cal_f1 = 2.0 * TPR / (TPR + (1.0 / r0) * FPR + 1)
     avg = (f1 + ba + cal_f1 + mcc) / 4.0
@@ -129,48 +134,36 @@ def main(args):
 
     if args.verbose:
         print(f'Final dataframe = {df.shape}', file=sys.stderr)
+        print(df.describe(), file=sys.stderr)
 
-    ref = df['manual_label']
+    y = df['manual_label'].copy()
 
     if args.verbose:
-        x = ref.unique()
-        print(f'unique(ref): {x}', file=sys.stderr)
+        print('Y=', file=sys.stderr)
+        print(y.describe(), file=sys.stderr)
+        print(f'unique(y): {y.unique()}', file=sys.stderr)
 
-    # Replace NAN's with 0's
-    df = df.fillna(0)
-    # Replace 'unknown' with 'unclassified'
-    df = df.replace(1.0, 0.0)
-    # Replace 'water column' with 'unclassified'
-    df = df.replace(45.0, 0.0)
-
-    n_total = len(df.index)
-    n_noise = len(df[df.manual_label == 0].index)
-    n_bathy = len(df[df.manual_label == 40].index)
-    n_surface = len(df[df.manual_label == 41].index)
-    print(f'total photons {n_total}', file=sys.stderr)
-    print(f'total noise photons {n_noise}', file=sys.stderr)
-    print(f'total bathy photons {n_bathy}', file=sys.stderr)
-    print(f'total surface photons {n_surface}', file=sys.stderr)
-
+    # Score each algorithm
     for n, a in enumerate(algorithms):
+
         headers = True if n == 0 else False
 
         if args.verbose:
             print(f'Scoring {a}', file=sys.stderr)
 
         if args.all:
-            score_all('all', a, ref, df, headers)
+            score_all('all', a, y, df, headers)
         else:
             # Remove photons labeled as surface
-            df2 = df[df.manual_label != 41]
-            ref2 = df2['manual_label']
+            df2 = df[df.manual_label != 41].copy()
+            y2 = df2['manual_label'].copy()
             if args.verbose:
-                print(f'Removed {n_total-len(df2.index)} surface photons',
+                print(f'Removed {len(df)-len(df2.index)} surface photons',
                       file=sys.stderr)
 
-            score_binary('surface', a, ref, df, 41, headers)
-            score_binary('bathy', a, ref, df, 40)
-            score_binary('nonsurface', a, ref2, df2, 40)
+            score_binary('surface', a, y, df, 41, headers)
+            score_binary('bathy', a, y, df, 40)
+            score_binary('nonsurface', a, y2, df2, 40)
 
 
 if __name__ == "__main__":
